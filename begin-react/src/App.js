58b514444ded8a405bdcbae8a39e8ada84d7af5b
이예진 <dldpwls3000@gmail.com>
2023-08-27 02:11:24 +0900

    Hello.js 추가 및 Fragment 사용 테스트

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hello from './Hello';
 
 function App() {
+  /*
+  2개 이상의 태그는 하나의 태그 안에 감싸져야 한다.
+  불필요한 태그를 붙일 경우 스타일이 틀어지는 경우가 있을 수 있다.
+  그래서 Fragment를 사용하면 불필요한 태그가 없음 
+  Fragment 작성 : 이름없는 태그
+  */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Hello />
+      <div>안녕하세요</div>
+    </>
   );
 }
 
