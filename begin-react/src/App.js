be9e40150f33df385cae02ab139bb00439c1078d
이예진 <dldpwls3000@gmail.com>
2023-08-28 01:22:24 +0900

    변수, 스타일 적용

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
+// 변수, 스타일 적용
 function App() {
-  /*
-  2개 이상의 태그는 하나의 태그 안에 감싸져야 한다.
-  불필요한 태그를 붙일 경우 스타일이 틀어지는 경우가 있을 수 있다.
-  그래서 Fragment를 사용하면 불필요한 태그가 없음 
-  Fragment 작성 : 이름없는 태그
-  */
-
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '15px',
+  }
   return (
     <>
       <Hello />
-      <div>안녕하세요</div>
+      <div style={style}>{name}</div>
+      {/* class 이름을 설정할때는 className으로 */}
+      <div className="gray-box"></div>
     </>
   );
 }
