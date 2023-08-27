d5e30b95909f74ec1fa26935672277a2bfed6333
이예진 <dldpwls3000@gmail.com>
2023-08-28 02:03:13 +0900

    조건부 렌더링

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,23 +1,14 @@
 import React from 'react';
-
-// 쓰이는 곳(App.js)에서 값을 정한다 = props(부모)
-// props 사용 방법 1
-/*
-function Hello(props) {
-    return <div style={{
-        color: props.color
-    }}>안녕하세요 {props.name}</div>;
-}
-*/
-
-// props 사용 방법 2
-function Hello({ color, name }) {
-    return <div style={{
-        color
-    }}>안녕하세요 {name}</div>;
+function Hello({ color, name, isSpecial }) {
+    return ( 
+        <div style={{ color }}>
+            {/* { isSpecial ? <b>*</b> : null } */}
+            { isSpecial && <b>*</b> }
+            안녕하세요 {name}
+        </div>
+    );
 }
 
-// 설정된 props가 없는 경우 default 값 설정
 Hello.defaultProps = {
     name:"이름없음",
 };
