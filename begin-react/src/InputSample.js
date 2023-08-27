3f5fe22a1d69956592ca571199e5c86add8ca125
이예진 <dldpwls3000@gmail.com>
2023-08-28 02:32:52 +0900

    여러개 input 상태 관리

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,22 +1,43 @@
 import React, { useState } from 'react';
 
+// #의 작업을 "불편성을 지킨다"라고 함
+// 불변성을 지켜주어야만 컴포넌트 업데이트 성능 최적화를 제대로 할 수 있음 
 function InputSample() {
-    const [text, setText] = useState('');
+    // #객체 상태 업데이트
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: ''
+    });
+
+    const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
     const onChange = (e) => {
-        setText(e.target.value);
+        // #기존의 상태 복사
+        const { value, name } = e.target; // e.target 에서 name 과 value 를 추출
+        
+        // #값을 덮어씌우고 새로운 값 설정
+        setInputs({
+        ...inputs, // 기존의 input 객체를 복사
+        [name]: value // name 키를 가진 값을 value 로 설정 (nextInputs[name] = value와 같은 동작)
+        });
     };
 
     const onReset = () => {
-        setText('');
+        setInputs({
+        name: '',
+        nickname: '',
+        })
     };
+
 
     return (
         <div>
-        <input onChange={onChange} value={text}/>
+        <input name="name" placeholder="이름" onChange={onChange} value={name} />
+        <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
         <button onClick={onReset}>초기화</button>
         <div>
-            <b>값: {text}</b>
+            <b>값: </b>
+            {name} ({nickname})
         </div>
         </div>
     );
