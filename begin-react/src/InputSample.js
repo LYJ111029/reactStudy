69852c61e17e81046e6df3ce739af07f4513654c
이예진 <dldpwls3000@gmail.com>
2023-08-29 00:53:04 +0900

    useRef를 사용하여 dom에 직접 접근해서 처리
    초기화 버튼을 클릭하면 이름input에 포커스가 가도록 처리

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,24 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
-// #의 작업을 "불편성을 지킨다"라고 함
-// 불변성을 지켜주어야만 컴포넌트 업데이트 성능 최적화를 제대로 할 수 있음 
+// dom에 직접 접근
 function InputSample() {
-    // #객체 상태 업데이트
     const [inputs, setInputs] = useState({
         name: '',
         nickname: ''
     });
 
-    const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
+    const nameInput = useRef();
+    const { name, nickname } = inputs;
 
     const onChange = (e) => {
-        // #기존의 상태 복사
-        const { value, name } = e.target; // e.target 에서 name 과 value 를 추출
+        const { value, name } = e.target; 
         
-        // #값을 덮어씌우고 새로운 값 설정
         setInputs({
-        ...inputs, // 기존의 input 객체를 복사
-        [name]: value // name 키를 가진 값을 value 로 설정 (nextInputs[name] = value와 같은 동작)
+        ...inputs,
+        [name]: value 
         });
     };
 
@@ -26,13 +23,14 @@ function InputSample() {
         setInputs({
         name: '',
         nickname: '',
-        })
+        });
+        nameInput.current.focus()
     };
 
 
     return (
         <div>
-        <input name="name" placeholder="이름" onChange={onChange} value={name} />
+        <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput}/>
         <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
         <button onClick={onReset}>초기화</button>
         <div>
