80f989f3f5d2e3412d243ed2d5a1df0d7463edac
이예진 <dldpwls3000@gmail.com>
2023-08-29 22:21:05 +0900

    배열 항목 추가하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  return (
-    <UserList />
-  );
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: ''
+    });
+    const { username, email } = inputs;
+    const onChange = e => {
+        const { name, value } = e.target;
+        setInputs({
+          ...inputs,
+          [name]: value
+        });
+    };
+    const [users, setUsers] = useState([
+        {
+            id: 1,
+            username: 'velopert',
+            email: 'public.velopert@gmail.com'
+        },
+        {
+            id: 2,
+            username: 'tester',
+            email: 'tester@example.com'
+        },
+        {
+            id: 3,
+            username: 'liz',
+            email: 'liz@example.com'
+        }
+    ]);
+    //useRef를 사용한 것은 값이 변하더라도 리렌더링 되지 않는다.
+    const nextId = useRef(4);
+    const onCreate = () => {
+        const user = {
+            id: nextId.current,
+            username,
+            email,
+        };
+        // 배열 추가 방법 2가지 (스프레트 연산자 또는 concat 사용)
+        setUsers([...users, user]);
+        // setUsers(users.concat(user));
+
+        setInputs({
+            username: '',
+            email: '',
+        });
+        nextId.current += 1;
+    };
+    return (
+        <>
+            <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+            <UserList users={users} />
+        </>
+    );
 }
 
 export default App;
