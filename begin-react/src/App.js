edf400b7a5bc39ba0a5ad2806666ae045ec33cca
이예진 <dldpwls3000@gmail.com>
2023-09-04 23:13:44 +0900

    배열 항목 수정하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -19,17 +19,20 @@ function App() {
         {
             id: 1,
             username: 'velopert',
-            email: 'public.velopert@gmail.com'
+            email: 'public.velopert@gmail.com',
+            active: true,
         },
         {
             id: 2,
             username: 'tester',
-            email: 'tester@example.com'
+            email: 'tester@example.com',
+            active: true,
         },
         {
             id: 3,
             username: 'liz',
-            email: 'liz@example.com'
+            email: 'liz@example.com',
+            active: true,
         }
     ]);
     //useRef를 사용한 것은 값이 변하더라도 리렌더링 되지 않는다.
@@ -54,10 +57,18 @@ function App() {
     const onRemove = id => {
         setUsers(users.filter(user => user.id !== id));
     }
+
+    const onToggle = id => {
+        setUsers(users.map(
+            user => user.id === id
+                ? { ...user, active: !user.active }
+                : user
+        ))
+    }
     return (
         <>
             <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
-            <UserList users={users} onRemove={onRemove}/>
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
         </>
     );
 }
--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,22 +1,26 @@
 import React from 'react';
 
-function User({ user, onRemove }) {
-    const {username, email, id} = user;
+function User({ user, onRemove, onToggle }) {
+    const {username, email, id, active } = user;
     return (
         <div>
-            <b>{username}</b> <span>({email})</span>
+            <b 
+                style={{
+                    cursor: 'pointer',
+                    color: active ? 'green' : 'black'
+                }}
+                onClick={() => onToggle(id)}
+            >{username}</b> <span>({email})</span>
             <button onClick={() => onRemove(id)}>삭제</button>
         </div>
     );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
     return (
         <div>
-            {/* 배열을 렌더링할때는 key(고유값) 설정해줘야함 */}
-            {/* 고유값이 따로 없다면, index를 key로 사용 */}
-            {users.map((user, index) => (
-                <User user={user} key={user.id} onRemove={onRemove}/>
+            {users.map((user) => (
+                <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
             ))}
         </div>
     );
