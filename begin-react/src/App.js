d7dff0722cbefadc3500c3d1a42e62c37c35c26c
이예진 <dldpwls3000@gmail.com>
2023-09-21 20:51:52 +0900

    scss를 이용한 버튼 예제들

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,74 +1,58 @@
-import React, { useReducer, useMemo } from 'react';
-import UserList from './UserList';
-import CreateUser from './CreateUser';
-import { produce } from 'immer';
-
-function countActiveUsers(users) {
-    console.log('활성 사용자 수를 세는중...');
-    return users.filter((user) => user.active).length;
-}
-
-const initialState = {
-    users: [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com',
-            active: true,
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com',
-            active: false,
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com',
-            active: false,
-        },
-    ],
-};
-
-function reducer(state, action) {
-    switch (action.type) {
-        case 'CREATE_USER':
-            return produce(state, (draft) => {
-                draft.users.push(action.user);
-            });
-        case 'TOGGLE_USER':
-            return produce(state, (draft) => {
-                const user = draft.users.find((user) => user.id === action.id);
-                user.active = !user.active;
-            });
-        case 'REMOVE_USER':
-            return produce(state, (draft) => {
-                const index = draft.users.findIndex(
-                    (user) => user.id === action.id
-                );
-                draft.users.splice(index, 1);
-            });
-        default:
-            return state;
-    }
-}
-
-// UserDispatch 라는 이름으로 내보내줍니다.
-export const UserDispatch = React.createContext(null);
+import React from 'react';
+import './App.scss';
+import Button from './component/Button';
 
 function App() {
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    const { users } = state;
-
-    const count = useMemo(() => countActiveUsers(users), [users]);
     return (
-        <UserDispatch.Provider value={dispatch}>
-            <CreateUser />
-            <UserList users={users} />
-            <div>활성사용자 수 : {count}</div>
-        </UserDispatch.Provider>
+        <div className="App">
+            <div className="buttons">
+                <Button size="large" onClick={() => console.log('클릭됐다!')}>
+                    BUTTON
+                </Button>
+                <Button>BUTTON</Button>
+                <Button size="small">BUTTON</Button>
+            </div>
+            <div className="buttons">
+                <Button size="large" color="gray">
+                    BUTTON
+                </Button>
+                <Button color="gray">BUTTON</Button>
+                <Button size="small" color="gray">
+                    BUTTON
+                </Button>
+            </div>
+            <div className="buttons">
+                <Button size="large" color="pink">
+                    BUTTON
+                </Button>
+                <Button color="pink">BUTTON</Button>
+                <Button size="small" color="pink">
+                    BUTTON
+                </Button>
+            </div>
+            <div className="buttons">
+                <Button size="large" color="blue" outline>
+                    BUTTON
+                </Button>
+                <Button color="gray" outline>
+                    BUTTON
+                </Button>
+                <Button size="small" color="pink" outline>
+                    BUTTON
+                </Button>
+            </div>
+            <div className="buttons">
+                <Button size="large" fullWidth>
+                    BUTTON
+                </Button>
+                <Button size="large" color="gray" fullWidth>
+                    BUTTON
+                </Button>
+                <Button size="large" color="pink" fullWidth>
+                    BUTTON
+                </Button>
+            </div>
+        </div>
     );
 }
 
